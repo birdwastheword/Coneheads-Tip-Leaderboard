0fb19efeafc8a6a5577e345148683dc3b5da58cd
birdwastheword <141559997+birdwastheword@users.noreply.github.com>
2023-08-06 15:04:49 +0200

    Get JWT from Reddit.

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello, World!")
+import http.client
+import json
+import os
+import urllib.parse
+import base64
+
+client_id = os.environ["CLIENT_ID"]
+cliend_secret = os.environ["CLIENT_SECRET"]
+username = os.environ["REDDIT_NAME"]
+password = os.environ["PASSWORD"]
+token = base64.b64encode(f"{client_id}:{cliend_secret}".encode('utf-8')).decode("ascii")
+
+conn = http.client.HTTPSConnection("www.reddit.com")
+payload = f'grant_type=password&username={urllib.parse.quote_plus(username)}&password={urllib.parse.quote_plus(password)}'
+headers = {
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'User-Agent': f'avatar_bot_reader/0.1 by {username}',
+  'Authorization': f'Basic {token}',
+}
+conn.request("POST", "/api/v1/access_token", payload, headers)
+res = conn.getresponse()
+data = res.read().decode("utf-8")
+jwt = json.loads(data)["access_token"]
+
+print(jwt)
