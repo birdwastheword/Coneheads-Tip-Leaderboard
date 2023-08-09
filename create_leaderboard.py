13e91ed7470674c69e536f0fdfc97aa9e9b15fa8
birdwastheword <141559997+birdwastheword@users.noreply.github.com>
2023-08-09 09:26:43 +0200

    Add Hall of Fame generation.

--- create_leaderboard.py
+++ create_leaderboard.py
@@ -4,6 +4,7 @@ import csv
 import datetime
 
 totals = defaultdict(lambda:0,{})
+hall_of_fame = []
 
 def process(filename):
   with open(filename) as csv_file:
@@ -14,10 +15,15 @@ def process(filename):
         #skip header
         line_count += 1
       else:
+        utc = row[0]
         fromUser = row[1]
+        toUser = row[2]
         amount = int(row[3])
         line_count += 1
         totals[fromUser] += amount
+        if amount >= 1000000:
+          global hall_of_fame
+          hall_of_fame.append((utc, fromUser, toUser, amount))
   print(f'Processed    {line_count}    lines from {filename}.')
 
 directory = os.fsencode("runs")
@@ -25,6 +31,7 @@ for file in os.listdir(directory):
   filename = f"runs/{os.fsdecode(file)}/tips.csv"
   process(filename)
 
+#Create leaderboard
 totals = (dict(sorted(totals.items(), key=lambda item: -item[1])))
 rank = 0
 leaderboard = f"Last updated: {datetime.datetime.now().strftime('%y-%m-%d %H:%M:%S')} UTC  \r\nRank | Username | Totals Tips\r\n:-|:-|-:\r\n"
@@ -35,5 +42,21 @@ for name, total in totals.items():
 filename = "leaderboard-total.md"
 f = open(filename, "w")
 f.write(leaderboard)
+print(f"written to {f.name}")
+f.close()
+
+#Create Hall of Fame
+hall_sorted = sorted(hall_of_fame, key=lambda t: (-t[3], t[0]))
+rank = 0
+hall_text = "Tips Hall of Fame:\r\n\rRank | Date | Username | Totals Tips | to User\r\n:-|:-|:-|-:|:-\r\n"
+for (date, fromUser, toUser, amount) in hall_sorted:
+  rank += 1
+  hall_text += f"{rank} | {date} | {fromUser.removeprefix(' /u/')} | {'{:,}'.format(amount)}| {toUser.removeprefix(' /u/')}\r\n"
+
+hall_text += "\r\nTips over 1,000,000 will be eligible for the Hall of Fame\r\n"
+
+filename = "hall-of-fame.md"
+f = open(filename, "w")
+f.write(hall_text)
 print(f"written to {f.name}")
 f.close()
