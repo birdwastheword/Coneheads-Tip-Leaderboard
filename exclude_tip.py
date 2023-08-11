a37906c79bc2a789e09d11af1813a5fcb6673b9f
birdwastheword <141559997+birdwastheword@users.noreply.github.com>
2023-08-11 18:01:10 +0200

    Add exclude list for tips to be removed

---
+++ exclude_tip.py
@@ -0,0 +1,7 @@
+# These are tips that are not part of the leaderboard and hall of fame.
+
+exclude_tip = {
+    ('2023-08-10 22:08:50', '/u/Wonderful_Bad6531') : 'This Tip was a payment',
+    ('2023-08-11 10:36:30', '/u/LuminousViper') : 'This Tip was a payment, that was refunded',
+    ('2023-08-11 11:28:33', '/u/possibili-teas') : 'This Tip is a refund of a payment'
+}
