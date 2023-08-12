73288548d4f1f15f2c643a7e66896635e30d9923
birdwastheword <141559997+birdwastheword@users.noreply.github.com>
2023-08-12 22:44:17 +0200

    suspended most tip excludes
    We will need to figure out first how to handle "fair play"

--- exclude_tip.py
+++ exclude_tip.py
@@ -1,9 +1,10 @@
 # These are tips that are not part of the leaderboard and hall of fame.
 
 exclude_tip = {
-    ('2023-07-29 10:14:39', '/u/Wonderful_Bad6531') : 'This Tip was a payment',
-    ('2023-08-10 22:08:50', '/u/Wonderful_Bad6531') : 'This Tip was a payment',
+    #('2023-07-29 10:14:39', '/u/Wonderful_Bad6531') : 'This Tip was a payment',
+    #('2023-08-10 22:08:50', '/u/Wonderful_Bad6531') : 'This Tip was a payment',
     ('2023-08-11 10:36:30', '/u/LuminousViper') : 'This Tip was a payment, that was refunded',
-    ('2023-08-11 11:28:33', '/u/possibili-teas') : 'This Tip is a refund of a payment',
-    ('2023-08-12 07:48:04', '/u/Wonderful_Bad6531') : 'This Tip was a payment for an avatar'
+    ('2023-08-11 11:28:33', '/u/possibili-teas') : 'This Tip is a refund of a payment'
+    #('2023-08-12 18:29:28', '/u/birdwastheword') : 'Repayed on chain'
+    #('2023-08-12 07:48:04', '/u/Wonderful_Bad6531') : 'This Tip was a payment for an avatar'
 }
