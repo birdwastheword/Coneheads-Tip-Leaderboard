53a3cf19db42848e4d826e3e781f75e33bc74e8c
birdwastheword <141559997+birdwastheword@users.noreply.github.com>
2023-08-12 21:55:19 +0200

    Update exclude_tip.py
    Removed the on chain one, since those seem ok for now.

--- exclude_tip.py
+++ exclude_tip.py
@@ -5,6 +5,5 @@ exclude_tip = {
     ('2023-08-10 22:08:50', '/u/Wonderful_Bad6531') : 'This Tip was a payment',
     ('2023-08-11 10:36:30', '/u/LuminousViper') : 'This Tip was a payment, that was refunded',
     ('2023-08-11 11:28:33', '/u/possibili-teas') : 'This Tip is a refund of a payment',
-    ('2023-08-12 07:48:04', '/u/Wonderful_Bad6531') : 'This Tip was a payment',
-    ('2023-08-12 18:29:28', '/u/birdwastheword') : 'Repayed on chain'
+    ('2023-08-12 07:48:04', '/u/Wonderful_Bad6531') : 'This Tip was a payment for an avatar'
 }
