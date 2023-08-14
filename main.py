a9e2bd290e1d1018ecbcca3b7d787897e0e40c83
birdwastheword <141559997+birdwastheword@users.noreply.github.com>
2023-08-14 17:13:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,21 @@
 import http.client
-import json
-import os
 import urllib.parse
-import base64
+import reddit_oauth
 import datetime
 from collections import defaultdict
 import csv
 
-# Set up env
-client_id = os.environ["CLIENT_ID"]
-cliend_secret = os.environ["CLIENT_SECRET"]
-username = os.environ["REDDIT_NAME"]
-password = os.environ["PASSWORD"]
-token = base64.b64encode(f"{client_id}:{cliend_secret}".encode('utf-8')).decode("ascii")
+# Set up
 yesterday = datetime.datetime.now() - datetime.timedelta(days = 1)
-
-# Obtain JTW
-conn = http.client.HTTPSConnection("www.reddit.com")
-payload = f'grant_type=password&username={urllib.parse.quote_plus(username)}&password={urllib.parse.quote_plus(password)}'
-headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'User-Agent': f'avatar_bot_reader/0.1 by {username}',
-    'Authorization': f'Basic {token}',
-}
-conn.request("POST", "/api/v1/access_token", payload, headers)
-res = conn.getresponse()
-data = res.read().decode("utf-8")
-jwt = json.loads(data)["access_token"]
-
-print(f"jwt obtained: {jwt}")
-
-# Get avatar bot comments
-conn = http.client.HTTPSConnection("oauth.reddit.com")
-payload = ''
-headers = {
-    'User-Agent': f'avatar_bot_reader/0.1 by {username}',
-    'Authorization': f'Bearer {jwt}'
-}
-
-# Process a batch of avatar bot comments.
 totals = defaultdict(lambda:0,{})
 biggest = ("", "", 0)
 
+# Process cvs with yesterdays tips
 filename = f"runs/{yesterday.date()}/tips.csv"
 with open(filename) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
-        if row[0] == 'utc':
-            #skip header
-            day_of_tips = {}
-        else:
+        if row[0] != 'utc': #skip header
             fromUser = row[1]
             toUser = row[2]
             amount = int(row[3])
@@ -71,11 +37,18 @@ for name, total in totals.items():
 
 comment += f"\r\nThis day is defined starting at {yesterday.date()} 00:00 UTC and ending at {datetime.datetime.now().date()} 00:00 UTC.  \r\n" \
            f"The code generating this leaderboard is available for review here: https://github.com/birdwastheword/Coneheads-Tip-Leaderboard"
-print(comment)
 
 title=f"Daily Tipping Leaderboard {yesterday.strftime('%B %-d')} (Unofficial)"
-payload = f'sr=ConeHeads&kind=self&title={urllib.parse.quote_plus(title)}&flair_id=d5c93210-4062-11ed-8995-3222414e6f3b&text={urllib.parse.quote_plus(comment)}'
 
 print(title)
-conn.request("POST", "/api/submit", payload, headers)
+print(comment)
+
+jwt = reddit_oauth.jwt()
+conn = http.client.HTTPSConnection("oauth.reddit.com")
+headers = {
+    'User-Agent': f'avatar_bot_reader/0.1 by {reddit_oauth.username}',
+    'Authorization': f'Bearer {jwt}'
+}
+payload = f'sr=ConeHeads&kind=self&title={urllib.parse.quote_plus(title)}&flair_id=d5c93210-4062-11ed-8995-3222414e6f3b&text={urllib.parse.quote_plus(comment)}'
+conn.request("POST", "/api/submit", '', headers)
 print(conn.getresponse().read().decode("utf-8"))
