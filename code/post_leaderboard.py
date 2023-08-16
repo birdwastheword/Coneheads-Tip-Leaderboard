3adca272593d31ae7afb3bdbdd3be10736708c7c
birdwastheword <141559997+birdwastheword@users.noreply.github.com>
2023-08-17 00:42:03 +0200

    Small test changes in leaderboard daily post.

--- code/post_leaderboard.py
+++ code/post_leaderboard.py
@@ -28,15 +28,17 @@ totals = (dict(sorted(totals.items(), key=lambda item: -item[1])))
 first = list(totals.items())[0]
 
 rank = 0
-comment = f"\r\nSince the official tipping leaderboard is still under construction I created a tipping leaderboard for just a single day. " \
-          f"Congratulation to {first[0]} tipping {'{:,}'.format(first[1])} put you in the #1 spot. The biggest tip was from {biggest[0]} tipping {'{:,}'.format(biggest[2])} to {biggest[1]}\r\n" \
+comment = f"🫣Teaser time🫣 the \"All Time Leaderboard\" will be ready soon..  \r\n" \
+          f"Congratulation to{first[0]} tipping {'{:,}'.format(first[1])} put you in the #1 spot.  \r\n" \
+          f"The biggest tip was from{biggest[0]} tipping {'{:,}'.format(biggest[2])} to{biggest[1]}\r\n" \
           "\r\nRank | Username | Totals Tips\r\n:-|:-|-:\r\n"
 for name, total in totals.items():
   rank += 1
   comment += f"{rank} | {name.removeprefix(' /u/')} | {'{:,}'.format(total)}\r\n"
 
 comment += f"\r\nThis day is defined starting at {yesterday.date()} 00:00 UTC and ending at {datetime.datetime.now().date()} 00:00 UTC.  \r\n" \
-           f"The code generating this leaderboard is available for review here: https://github.com/birdwastheword/Coneheads-Tip-Leaderboard"
+           f"The code generating this leaderboard is available for review here:  \r\n" \
+           f"`https://github.com/birdwastheword/Coneheads-Tip-Leaderboard`"
 
 title=f"Daily Tipping Leaderboard {yesterday.strftime('%B %-d')} (Unofficial)"
 
